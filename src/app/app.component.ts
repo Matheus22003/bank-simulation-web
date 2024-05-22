4bc57a4836a799a080279357f73677785922fb93
Matheus de Oliveira Simões <45323213+Matheus22003@users.noreply.github.com>
2024-05-21 21:45:59 -0300

    Projeto bank-simulation-web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {setTheme} from "ngx-bootstrap/utils";
+import {NavbarComponent} from "./core/main/navbar/navbar.component";
+import {AuthService} from "./core/services/auth.service";
+import {UsuarioModel} from "./core/models/usuario.model";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bank-simulation-web';
+
+  constructor() {
+    setTheme('bs5');
+  }
+
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LoginComponent} from "./core/features/login/login.component";
+import {RegistrarComponent} from "./core/features/registrar/registrar.component";
+import {HomeComponent} from "./core/features/home/home.component";
+import {authGuard} from "./core/Guards/auth.guard";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'registrar', component: RegistrarComponent},
+  {path: 'home', component: HomeComponent, canActivate: [authGuard]},
+
+];
